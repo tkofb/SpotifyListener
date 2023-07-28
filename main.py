855dbb48d8e0d715f7502670cf57fc94845fd72b
tkofb <benard.kihiuria@gmail.com>
2023-07-28 14:16:28 -0400

    Stuck on why this isn't working as the endpoints are returning correct

--- main.py
+++ main.py
@@ -1,60 +1,34 @@
-#Testing auto use password
 from dotenv import load_dotenv
 import os
-import base64
 import json
-from requests import post, get
+import requests
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+clientId = os.getenv("CLIENT_ID")
+clientSecret = os.getenv("CLIENT_SECRET")
+tuiClientId =  os.getenv("TUI_CLIENT_ID")
+tuiClientSecret = os.getenv("TUI_CLIENT_SECRET")
 
-def getToken():
-    authorizationString = client_id + ":" + client_secret
-    authorizationBytes = authorizationString.encode("utf-8")
-    authorizationBase64 = str(base64.b64encode(authorizationBytes), "utf-8")
+manualURL = "https://api.spotify.com/authorize?client_id=a56254b176ed4cf3b19c23b8c01c8507&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fcallback"
+scope = "&scope=user-read-currently-playing"
 
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + authorizationBase64,
-        "Content-Type": "application/x-www-form-urlencoded"
+def requestUserAuthorization():
+    url = "http://api.spotify.com/authorize"
+
+    query = {
+        "client_id": clientId,
+        "response_type": "code",
+        "redirect_uri": "http://localhost:8888/callback",
+        "scope" : "user-read-currently-playing"
     }
-    data = {"grant_type": "client_credentials"}
 
-    result = post(url, headers=headers, data=data)
-    jsonResult = json.loads(result.content)
-    token = jsonResult["access_token"]
-    return token
+    result = requests.get(url, params=query)
+    
+    print(result.request.url == url + scope)
+    print(result.request.url)
+    print(manualURL+scope)
+    print(query)
+    print(result.status_code)
 
-def getAuthorizationHeader(token):
-    return {"Authorization": "Bearer " + token}
-
-def searchForArtist(token, artistName):
-    url = "https://api.spotify.com/v1/search"
-    headers = getAuthorizationHeader(token)
-    query = f"?q={artistName}&type=artist&limit=1"
-
-    queryURL = url + query
-    result = get(queryURL, headers=headers)
-    jsonResult = json.loads(result.content)["artists"]["items"]
-    if len(jsonResult) == 0:
-        print("No Sauze")
-        return None
-    return jsonResult[0]
-
-def getSongsByArtist(token,artistID):
-    url = "https://api.spotify.com/v1/artists/{}/top-tracks?country=US".format(artistID)
-    headers = getAuthorizationHeader(token)
-    result = get(url, headers = headers)
-    jsonResult = json.loads(result.content)["tracks"]
-    return jsonResult
-
-token = getToken()
-artist = searchForArtist(token, "Frank Ocean")
-artistID = artist["id"]
-print(artistID)
-songs = getSongsByArtist(token, artistID)
-
-for i, song in enumerate(songs):
-    print("{}. {}".format(i+1,song['name']))
+requestUserAuthorization()
---
+++ tutorial.py
@@ -0,0 +1,60 @@
+#Testing auto use password
+from dotenv import load_dotenv
+import os
+import base64
+import json
+from requests import post, get
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def getToken():
+    authorizationString = client_id + ":" + client_secret
+    authorizationBytes = authorizationString.encode("utf-8")
+    authorizationBase64 = str(base64.b64encode(authorizationBytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + authorizationBase64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+
+    result = post(url, headers=headers, data=data)
+    jsonResult = json.loads(result.content)
+    token = jsonResult["access_token"]
+    return token
+
+def getAuthorizationHeader(token):
+    return {"Authorization": "Bearer " + token}
+
+def searchForArtist(token, artistName):
+    url = "https://api.spotify.com/v1/search"
+    headers = getAuthorizationHeader(token)
+    query = f"?q={artistName}&type=artist&limit=1"
+
+    queryURL = url + query
+    result = get(queryURL, headers=headers)
+    jsonResult = json.loads(result.content)["artists"]["items"]
+    if len(jsonResult) == 0:
+        print("No Sauze")
+        return None
+    return jsonResult[0]
+
+def getSongsByArtist(token,artistID):
+    url = "https://api.spotify.com/v1/artists/{}/top-tracks?country=US".format(artistID)
+    headers = getAuthorizationHeader(token)
+    result = get(url, headers = headers)
+    jsonResult = json.loads(result.content)["tracks"]
+    return jsonResult
+
+token = getToken()
+artist = searchForArtist(token, "Frank Ocean")
+artistID = artist["id"]
+print(artistID)
+songs = getSongsByArtist(token, artistID)
+
+for i, song in enumerate(songs):
+    print("{}. {}".format(i+1,song['name']))
